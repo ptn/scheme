390128a0dba1d77d4297bd12aad90c1856e3a951
Pablo Torres <tn.pablo@gmail.com>
2014-07-04 13:18:31 -0500

    More tests

--- test/scheme/core_test.clj
+++ test/scheme/core_test.clj
@@ -16,7 +16,10 @@
   (is (= 3 (scope-get {"y" 4 :parent {"x" 3}} "x")))
   (try
     (scope-get {"y" 4 :parent {"x" 3}} "z")
-    (catch Exception e (is (= "Undefined z" (.getMessage e))))))
+    (catch Exception e (is (= "Undefined z" (.getMessage e)))))
+  (try
+    (sch-eval "(begin (lambda () (define x 2) x) x)")
+    (catch Exception e (is (= "Undefined x" (.getMessage e))))))
 
 (deftest eval-forms
   (is (= 1 (sch-eval "1")))
@@ -28,6 +31,8 @@
   (is (= (seq [2 3]) (sch-eval "(cdr (quote (1 2 3)))")))
   (is (= 4 (sch-eval "(begin (define x 3) (+ x 1))")))
   (is (= 2 (sch-eval "((lambda (x) x) 2)")))
+  (is (= 2 (sch-eval "((lambda () 2))")))
+  (is (= 3 (sch-eval "((lambda () (define x 2) (+ x 1)))")))
   (is (= 5 (sch-eval "(begin (define y 3) ((lambda (x) (+ x y)) 2))")))
   (is (= 7 (sch-eval "(if 1 (+ 3 4) (* 8 7))")))
   (is (= 10 (sch-eval "(if (+ 1 6) (+ 3 7) (* 8 7))")))
